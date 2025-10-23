02edf279d4b9c0364dc9aa84beb916fec67e7f4e
nalalisa <ehdgus8150@hanyang.ac.kr>
2025-10-24 00:09:57 +0900

    d

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306, // MySQL 기본 포트는 3306입니다.
   waitForConnections: true,
   connectionLimit: 10,
